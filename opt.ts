70fa783b7dde1ccd7ab7f4881c6c762875a37ed7
hashrock <hashrock@users.noreply.github.com>
2022-05-01 12:23:20 +0900

    doc: add deprication comments

--- opt.ts
+++ opt.ts
@@ -13,12 +13,13 @@
 
 import { open } from './mod.ts'
 
-
+/** @deprecated - use `OpenOptions` instead. */
 export interface OpnOptions {
     wait ?: boolean;
     app ?: string[];
 }
 
+/** @deprecated - use `open(target, options)` instead. */
 export function opn( target : string, options : OpnOptions = {} ){
     
     const { app , wait } = options;
